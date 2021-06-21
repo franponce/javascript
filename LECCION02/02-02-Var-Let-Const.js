becc8ddcd623048e70ba04c714d33c853d26d149
franponce <franponce98@gmail.com>
2021-06-21 18:39:51 -0300

    Diferencias

---
+++ LECCION02/02-02-Var-Let-Const.js
@@ -0,0 +1,10 @@
+var // Palabra reservada que ya no se usa
+
+// Ahora tenemos let y const 
+
+let // Declaramos variable
+
+const // Declaramos una constante. Una vez que le asigamos un valor a una variable, ya no se puede asignar un nuevo valor
+
+
+
