ba25a63a63a303adc78c74850a04d662a373af34
franponce <franponce98@gmail.com>
2021-06-21 17:58:04 -0300

    Tipos de datos más importantes string, numérico, object

---
+++ LECCION02/02-02-TiposDeDatosJavascript.js
@@ -0,0 +1,21 @@
+// Tipo de dato string
+
+var nombre = "Carlos";
+
+console.log(nombre);
+
+// Tipo de dato numerico
+
+var numero = 1000;
+
+console.log(numero);
+
+// Tipo de dato object 
+
+var objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+    telefono: "22334455"
+}
+
+console.log(objeto);
