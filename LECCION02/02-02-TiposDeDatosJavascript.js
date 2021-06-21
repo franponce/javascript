4af04070811e4b905fba93ef537abc96c19dc30e
franponce <franponce98@gmail.com>
2021-06-21 18:11:05 -0300

    Más de Tipos de Datos en JavaScript

--- LECCION02/02-02-TiposDeDatosJavascript.js
+++ LECCION02/02-02-TiposDeDatosJavascript.js
@@ -23,3 +23,47 @@ var objeto = {
 }
 
 console.log(objeto);
+
+// Tipo de dato boolean (true, false)
+
+var bandera = true;
+
+console.log(bandera);
+
+// Tipo de dato function 
+
+function miFuncion(){}
+
+console.log(miFuncion);
+
+// Tipo de dato Symbol. Crea un valor único de una variable
+
+var simbolo = Symbol("mi simbolo");
+
+console.log(simbolo);
+
+// Tipo clase es una function 
+
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(Persona);
+
+// Tipo undefined 
+
+var x;
+console.log(x);
+
+// null = ausencia de valor 
+
+var y = null
+
+console.log(y);
+
+x = undefined;
+
+console.log(x);
