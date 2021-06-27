85af3b9dd5a3428c250930e630f32606d6403993
franponce <franponce98@gmail.com>
2021-06-27 18:12:59 -0300

    Ejemplos de reglas para definir nombres

---
+++ 02-02-ReglasParaDefinirNombresDeVariables.js
@@ -0,0 +1,11 @@
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Lara";
+
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+//let 1nombreCompleto; no está permitidio inciiar el nombre de una vairable con un número 
